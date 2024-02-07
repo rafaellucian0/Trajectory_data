7ebc2530abb21335f0601bb721801e43384b278d
rafaellucian0 <rlls1@aluno.ifal.edu.br>
2024-02-07 02:08:21 -0300

    Change files to parquet

--- src/main.py
+++ src/main.py
@@ -10,22 +10,29 @@ def isdigit(input):
         return False
     return True
 
-def write_csv(path, bus_ids, bus_points):
-    #header de cada csv
-    header = ['id', 'loc', 'Semantica']
-    
-    for id in bus_ids:
+def write_file(bus_points, bus_days): 
+    #criar dataframe
+    #se o timestamp do item for de acordo com o timestamp da pasta, ele é adicionado ao dataframe
+    for timestamp in bus_days:
+        df = pd.DataFrame()   
+        for item in bus_points:
+            foldername=((item[2])[-1])[0:10]
 
-        #define o local de salvamento do arquivo
-        filename = os.path.join(path, f'{id}.csv') 
-        
-        with open(filename, 'w', newline="") as file:
-            #escreve info no csv
-            csvwriter = csv.writer(file)
-            csvwriter.writerow(header)
-            for item in bus_points:
-                if item[0]==id:
-                    csvwriter.writerow(item[1:])
+            if foldername == timestamp:
+                #define o local de salvamento do arquivo
+                path = rf"path\{timestamp}"
+
+                if not os.path.exists(path):
+                    os.makedirs(path)
+            
+                df = pd.concat([df, pd.DataFrame([item])], ignore_index=True)
+        print(df)
+    
+        #define o nome do arquivo parquet
+        filename = os.path.join(path, f'{item[0]}.parquet')
+    
+        #salva o df em parquet
+        df.to_parquet(filename)
 
 def convert_to_trajectory(path, line_code):
     #define a pasta dos arquivos 
@@ -33,14 +40,13 @@ def convert_to_trajectory(path, line_code):
     csv_files = [file for file in files if file.endswith('.csv')]
 
     #variáveis para verificar e agrupar os pontos pelo id
-    bus_ids = []
+    bus_days = []
     bus_points = []
 
-    id = 1
     size = 1
 
     for item in csv_files:
-        if size<100:
+        if size<=100:
             #transforma cada csv em df
             file_path = os.path.join(path, item)
             file = pd.read_csv(file_path)
@@ -52,8 +58,8 @@ def convert_to_trajectory(path, line_code):
                 if ((row['line_code'])==line_code or (row['line_code'])==str(line_code)):
 
                     #add o id do onibus pra agrupar os pontos dele em arquivos separados
-                    if row['bus_id'] not in bus_ids:   
-                        bus_ids.append(row['bus_id'])
+                    if (row['updated_at'])[:10] not in bus_days:
+                        bus_days.append((row['updated_at'])[:10])
 
                     Semantica = []
 
@@ -69,18 +75,19 @@ def convert_to_trajectory(path, line_code):
                     if isdigit(row['bus_direction']):
                         Semantica.append("direction: "+ str(int(row['bus_direction'])))
                     
-                    Semantica.append("timestamp: "+ str(row['updated_at']))
+                    Semantica.append(str(row['updated_at']))
 
                     #junta todos os atributos no formato P = (id, loc, semantica), o id do onibus é só para separar o arquivo depois
-                    point = (row['bus_id'], id, loc, Semantica)
+                    point = (row['bus_id'], loc, Semantica)
 
                     #junta os pontos no formato T = (p1, p2, ..., pi)
                     bus_points.append(point)
+                    
+            if size % 500 == 0:
+                print(size)
 
-                    id+=1
         size+=1
-
-    #escreve os pontos em arquivos csv
-    write_csv(r"path",bus_ids, bus_points)
+    #escreve os pontos em arquivos
+    write_file(bus_points, bus_days)
 
 convert_to_trajectory(r"path", 528)
