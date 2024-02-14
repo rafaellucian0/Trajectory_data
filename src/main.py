aa129756d937bb3c15102313e0d710d38ce6571a
rafaellucian0 <rlls1@aluno.ifal.edu.br>
2024-02-14 16:18:05 -0300

    Muda forma de agrupar os dados para "cidade/id_onibus.parquet"

--- src/main.py
+++ src/main.py
@@ -10,11 +10,13 @@ def isdigit(input):
     return True
 
 def write_file(bus_points): 
+    #PARQUET
+
     for key in bus_points:
-        path = rf"C:\Users\Rafael\Desktop\Rafael\UFAL\PIBIC\BRBus\Trajectory_data\trajectory_528\{key[:10]}"
+        path = rf"path"
         if not os.path.exists(path):
             os.makedirs(path)
-        filename = os.path.join(path, f'cur_{key[11:13]+key[-1]}.parquet')
+        filename = os.path.join(path, f'{key}.parquet')
 
         df = pd.DataFrame(bus_points[key])
 
@@ -28,39 +30,46 @@ def convert_to_trajectory(path):
 
     size = 1
     bus_points = {}
-
     for item in csv_files:
-        #transforma cada csv em df
-        file_path = os.path.join(path, item)
-        file = pd.read_csv(file_path)
-        for index, row in file.iterrows():
-            Semantica = []
+        if size <= 500:
+            #transforma cada csv em df
+            file_path = os.path.join(path, item)
+            file = pd.read_csv(file_path)
+            for index, row in file.iterrows():
+                Semantica = []
 
-            loc = [row['latitude'], row['longitude']]
-            #x = row['latitude']
-            #y = row['longitude']
+                #loc = [row['latitude'], row['longitude']]
+                x = row['latitude']
+                y = row['longitude']
 
-            #verifica se tem a info da velocidade
-            if isdigit(row['bus_speed']):
-                Semantica.append("speed: "+ str(int(row['bus_speed'])))
 
-            #verifica se tem a info da direcao
-            if isdigit(row['bus_direction']):
-                Semantica.append("direction: "+ str(int(row['bus_direction'])))
-            
-            Semantica.append(str(row['updated_at']))
 
-            #junta todos os atributos no formato P = (id, loc, semantica), o id do onibus é só para separar o arquivo depois
-            point = (row['bus_id'], loc, Semantica)
+                #verifica se tem a info da velocidade
+                if isdigit(row['bus_speed']):
+                    Semantica.append("speed: "+ str(int(row['bus_speed'])))
 
-            key = (row['updated_at'])[:15]
+                #verifica se tem a info da direcao
+                if isdigit(row['bus_direction']):
+                    Semantica.append("direction: "+ str(int(row['bus_direction'])))
+                
+                time = str(row['updated_at'])
 
-            if key in bus_points:
-                bus_points[key].append(point)
-            else:
-                bus_points[key] = [point]
-        print(size)
-        size+=1
+                #junta todos os atributos no formato P = (id, loc, semantica), o id do onibus é só para separar o arquivo depois
+
+                point = (x,y, time, Semantica)
+
+                #timestamp em parquet (olhar github)
+                key = row['bus_id']
+
+                if key in bus_points:
+                    bus_points[key].append(point)
+                else:
+                    bus_points[key] = [point]
+                    bus_points[key].append(point)
+                    
+            print(size)
+            size+=1
+    #print(bus_points)
     write_file(bus_points)
 
-convert_to_trajectory(r"C:\Users\Rafael\Downloads\curitiba")
+convert_to_trajectory(r"path")
