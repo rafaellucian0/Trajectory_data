10bddb88a72571a5553d532ed6eaa8c52a35972d
rafaellucian0 <rlls1@aluno.ifal.edu.br>
2024-02-27 15:26:17 -0300

    Corrige latitude e longitude

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import csv
 
 def isdigit(input):
     #checa se a entrada é digito
@@ -13,7 +14,7 @@ def write_file(bus_points):
     #PARQUET
 
     for key in bus_points:
-        path = rf"path"
+        path = rf"C:\Users\Rafael\Desktop\Rafael\UFAL\PIBIC\BRBus\Trajectory_data\trajectories_bus\curitiba"
         if not os.path.exists(path):
             os.makedirs(path)
         filename = os.path.join(path, f'{key}.parquet')
@@ -21,6 +22,36 @@ def write_file(bus_points):
         df = pd.DataFrame(bus_points[key])
 
         df.to_parquet(filename)
+
+    #CSV
+    #header = ['id', 'x', 'y', 'Semantica']
+    #path = rf"C:\Users\Rafael\Desktop\Rafael\UFAL\PIBIC\BRBus\Trajectory_data\csv_file"
+
+    #for key in bus_points:
+        #id = 1
+
+        #define o local de salvamento do arquivo
+        #filename = os.path.join(path, f'{key}.csv') 
+
+        #with open(filename, 'w', newline="") as file:
+            #escreve info no csv
+            #csvwriter = csv.writer(file)
+            #csvwriter.writerow(header)
+            #id = 1
+            #x_last = ""
+            #y_last = ""
+            #for point in bus_points[key]:
+                #print(point)
+                #return 0
+                #if point[0]!=x_last or y!=y_last:
+                    #for i in point[1]:
+                        #if i[:4]=='2023':
+                            #time = i[8:10]+'/'+i[5:7]+'/'+i[:4]+' '+i[11:19]
+                    #new_point = [id, float(x), float(y), time]
+                    #csvwriter.writerow(new_point)
+                    #id += 1 
+                    #x_last=x
+                    #y_last=y
 
 
 def convert_to_trajectory(path):
@@ -31,16 +62,16 @@ def convert_to_trajectory(path):
     size = 1
     bus_points = {}
     for item in csv_files:
-        if size <= 500:
+        if size <= 10000:
             #transforma cada csv em df
             file_path = os.path.join(path, item)
             file = pd.read_csv(file_path)
             for index, row in file.iterrows():
                 Semantica = []
 
-                #loc = [row['latitude'], row['longitude']]
-                x = row['latitude']
-                y = row['longitude']
+                loc = [row['latitude'], row['longitude']]
+                x = row['longitude']
+                y = row['latitude']
 
 
 
@@ -72,4 +103,4 @@ def convert_to_trajectory(path):
     #print(bus_points)
     write_file(bus_points)
 
-convert_to_trajectory(r"path")
+convert_to_trajectory(r"C:\Users\Rafael\Downloads\curitiba")
